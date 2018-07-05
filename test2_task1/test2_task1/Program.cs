5beaa3502d213f8af687ff5445ff07ee6ce8212f
rinsin1 <rinsin1@bigmir.net>
2018-07-05 17:37:45 +0300

    33333333333

--- test2_task1/test2_task1/Program.cs
+++ test2_task1/test2_task1/Program.cs
@@ -10,7 +10,7 @@ namespace test2_task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test CI_all_Branch_change");
+            Console.WriteLine("Test CI_all_Branch_3333");
         }
     }
 }
